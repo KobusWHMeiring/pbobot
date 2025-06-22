dd3ae3149b7fe8d53586e1d8255b4f85eabe20a0
Kobus Meiring <kobuswhmeiring@gmail.com>
2025-06-22 13:48:51 +0200

    naming refactor

--- pboproj/wsgi.py
+++ pboproj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pbobot project.
+WSGI config for pboproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pbobot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pboproj.settings')
 
 application = get_wsgi_application()
